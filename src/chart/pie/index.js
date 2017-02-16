47b0c7ced85167ca55f60fa962ba7c405495bcb9
liwensheng <527836355@qq.com>
2017-02-16 23:33:11 +0800

    饼图动画和datalabel模块

--- src/chart/pie/index.js
+++ src/chart/pie/index.js
@@ -20,15 +20,18 @@ Pie.prototype = {
 		var data = this.series.data;
 		var chart = this.chart;
 		var sum = cad.sum(data);
-		var colors = ['#00A1A1',"#28FFBB","#DB1774","#F20000"];
+		var colors = ['#00A1A1',"#28FFBB","#DB1774","#F20000","blue"];
 	    var cx = series.center[0]*chart.width;
 	    var cy = series.center[1]*chart.height;
-	    var radius = Math.min(chart.width,chart.height)*0.5;
-	    var startAngle = -90;
+	    var innerSize = series.innerSize;
+	    var size = series.size;
+	    var radius = Math.min(chart.width,chart.height)*size/2;
+	    var startAngle = series.startAngle - 90;
 	    var points = [];
+	    var totalAngle = series.endAngle - series.startAngle;
 	    data.reduce(function(start,end,index){
 	        var startAngle = start;
-	        var endAngle = startAngle + end/sum*360;
+	        var endAngle = startAngle + end/sum*(totalAngle);
 	        points.push({
 	        	startAngle:startAngle,
 	        	endAngle:endAngle,
@@ -38,7 +41,11 @@ Pie.prototype = {
 	        return endAngle;
 	    },startAngle);
 	    return {
-	    	points:points
+	    	points:points,
+	    	cx:cx,
+	    	cy:cy,
+	    	radius:radius,
+	    	innerRadius:radius*innerSize
 	    }
 	},
 	getDefaultSeries(){
@@ -48,11 +55,14 @@ Pie.prototype = {
 			borderWidth:1,
 			data:[],
 			dataLabels:{
-
+				enable:true,
+				inside:false
 			},
-			size:"75%",
+			size:0.75,
+			innerSize:0,
 			startAngle:0,
-			endAngle:null,
+			endAngle:360,
+			sliceOffset:10,
 			states:{
 				hover:{
 					enable:true
@@ -64,10 +74,23 @@ Pie.prototype = {
 		var points = this.state.points;
 		var paper = this.chart.getPaper();
 		paper.switchLayer(this.group);
+		//slice layer
+		var slice = paper.g();
+		var label = paper.g(); 
+		paper.switchLayer(slice);
 		points.map(function(p){
-			p.el = paper.path();
+			p.slice = paper.path();
+		});
+		paper.switchLayer(label);
+		points.map(function(p){
+			p.dataLabel = {
+				path:paper.path(),
+				text:paper.text()
+			}
 		});
 		this.refreshAttr();
+		this.animate();
+		this.attachEvent();
 		return this;
 	},
 	refreshAttr(){
@@ -75,26 +98,113 @@ Pie.prototype = {
 		var chart = this.chart;
 		var paper = chart.getPaper();
 		var {width,height} = chart;
-		var {center,size} = this.series;
+		var {center,size,dataLabels} = this.series;
 		var cx = width*center[0];
 		var cy = height*center[1];
-		var radius = Math.min(width,height)*0.335;//*size
-		points.map(function(p){
+		var {radius,innerRadius} =this.state;
+		points.map(function(p,index){
+			var {startAngle,endAngle} = p;
 			var path = cad.getShapePath("sector",cx,cy,{
 				startAngle:p.startAngle,
 				endAngle:p.endAngle,
 				radius:radius,
-				innerRadius:0
+				innerRadius:innerRadius
 			});
-			var el = p.el;
-			el.attr("d",path).fill(p.color);
+			var slice = p.slice;
+			slice.attr("d",path).fill(p.color);
+			var textPoint;
+			var dataLabel = p.dataLabel;
+			if(dataLabels.inside) {
+				textPoint = {x:cx,y:cy};
+				p.dataLabel.text.hide();
+			} else {
+				textPoint = cad.Point(cx,cy).angleMoveTo((startAngle+endAngle)/2,radius*1.3);
+			}
+			p.dataLabel.text.text(index)
+					 .attr("x",textPoint.x)
+					 .attr("y",textPoint.y)
+					 .attr("text-anchor","middle");
 		})
+	},
+	attachEvent(){
+		var that = this;
+		this.state.points.map(function(p,index){
+			p.slice.on("click",that.selectPoint.bind(that,index));
+			p.slice.on("mouseover",that.handleHover.bind(that,index,true));
+			p.slice.on("mouseout",that.handleHover.bind(that,index,false));
+		})
+	},
+	handleHover(index,isHover){
+		var point = this.state.points[index];
+		var color = point.color;
+		if(isHover) {
+			var hoverColor = cad.brighten(color,0.2);
+			point.slice.fill(hoverColor);
+		} else {
+			point.slice.fill(color);
+		}
+		if(this.series.dataLabels.inside) {
+			if(isHover) {
+				point.dataLabel.text.show();
+			} else {
+				point.dataLabel.text.hide();
+			}
+		} 
+	},
+	selectPoint(index){
+		var {points,cx,cy,radius} = this.state;
+		var point = points[index];
+		var {sliceOffset} = this.series;
+		var {startAngle,endAngle} = point;
+		if(!point.selected) {
+			var offset = cad.Point(0,0).angleMoveTo((startAngle+endAngle)/2,sliceOffset);
+			point.slice.translate(offset.x,offset.y);
+			point.selected = true;
+			points.map(function(p,key){
+				if(key!==index) {
+					p.slice.translate(0,0);
+					p.selected = false;
+				}
+			})		
+		} else {
+			point.selected = false;
+			point.slice.translate(0,0);
+		}
+
 	},
 	alignDataLabels(){
 
 	},
 	animate(){
-
+		var chart = this.chart;
+		var {cx,cy,radius} = this.state;
+		var {startAngle,endAngle} = this.series;
+		radius = Math.min(chart.width,chart.height);
+		var paper = this.chart.getPaper();
+		startAngle -= 90;
+		endAngle -= 90;
+		var group = this.group;
+		var clip = paper.clipPath(function(){
+			paper.sector(cx,cy,radius,startAngle,startAngle);
+		});
+		clip.attr("id","clip");
+		group.attr("clip-path","url(#clip)");
+		var path = clip.find("path");
+		path.transition({
+			from:startAngle,
+			to:endAngle,
+			ease:'easeOut',
+			during:1000,
+			onUpdate:function(val){
+				path.attr("d",cad.getShapePath("sector",cx,cy,{
+					startAngle:startAngle,
+					endAngle:val,
+					radius:radius
+				}));
+			},
+			callback:function(){
+			}
+		})
 	},
 	componentDidMount(){
 
--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@ import mychart from '../src/index.js'
 import '../../src/index'
 
 var el = document.getElementById("root");
+var t1 = Date.now();
 var chart = mychart.init(el,{width:600,height:400});
 chart.setOption({
 	chart:{
@@ -9,7 +10,17 @@ chart.setOption({
 	},
 	series:[
 		{
-			data:[55,22,12]
+			name:'a pie chart',
+			data:[11,11,11,12,11],
+			sliceOffset:20,
+			size:0.75,
+			innerSize:0,
+			center:[0.5,0.5],
+			dataLabels:{
+				inside:false
+			}
 		}
 	]
 });
+var t2 = Date.now();
+console.log('total-time:'+ (t2-t1)+'ms');
