9388e0453c81e2df419bb893774dfe17b85da586
liws <liws@mysoft.com.cn>
2017-04-06 10:53:52 +0800

    grid

--- src/component/grid.js
+++ src/component/grid.js
@@ -4,7 +4,23 @@ import cad from 'cad'
 
 class  Grid extends Component {
     getDefaultProps(){
-        
+        return {
+            option:{
+                    show:false,
+                    left:'auto',
+                    top:60,
+                    right:'10%',
+                    bottom:60,
+                    width:'auto',
+                    height:'auto',
+                    background:'transparent'
+                    labelWidth:null,
+            },
+            width:null,
+            height:null,
+            xAxis:[],
+            yAxis:[]
+        }
     }
     getInitialState(){
         
